842d07d94059b582ba4d5424e78698495b5659d0
Teddy Pascual <teddylayson2000@gmail.com>
2023-12-23 12:08:13 +0800

    connected front end to back end

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
 function App() {
+  const [backEndData, setBackEndData] = useState([{}]);
+  useEffect(() => {
+    fetch("/api/menu")
+      .then((response) => response.json())
+      .then((data) => setBackEndData(data));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {typeof backEndData === "undefined" ? (
+        <p>Loading...</p>
+      ) : (
+        backEndData.map((item) => <p key={item.id}>{item.name}</p>)
+      )}
     </div>
   );
 }
