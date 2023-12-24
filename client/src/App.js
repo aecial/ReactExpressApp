7b9d0bde58d276eeedf9550f9895e8895953b3e1
Teddy Pascual <teddylayson2000@gmail.com>
2023-12-24 12:16:04 +0800

    Implemented Menu Logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,36 @@
 import { useState, useEffect } from "react";
 import MenuCard from "./components/MenuCard";
-import Test from "./components/Test";
+import Cart from "./pages/Cart";
 function App() {
   const [backEndData, setBackEndData] = useState([{}]);
+  const [cartCounter, setCartCounter] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
   const [cartItem, setCartItem] = useState([]);
-  function addToCart(name) {
-    setCartItem((current) => [...current, name]);
+
+  function addToCart(name, price) {
+    setCartCounter((count) => count + 1);
+    const isFound = cartItem.some((el) => {
+      if (el.name === name) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    if (isFound) {
+      setCartItem((current) => {
+        return current.map((item) => {
+          if (item.name === name) {
+            const newPrice = price + item.price;
+            return { ...item, price: newPrice };
+          }
+
+          return item;
+        });
+      });
+    } else {
+      setCartItem((current) => [...current, { name, price }]);
+      console.log(cartItem);
+    }
   }
   useEffect(() => {
     fetch("/api/menu")
@@ -15,8 +40,8 @@ function App() {
 
   return (
     <div className="bg-slate-500 pb-4 text-white flex flex-col justify-center items-center">
-      <a
-        href="#"
+      <button
+        type="button"
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
       >
         CART
@@ -24,10 +49,10 @@ function App() {
           <span className="hidden"></span>
         ) : (
           <span className="inline-flex items-center justify-center w-4 h-4 ms-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
-            {cartItem.length}
+            {cartCounter}
           </span>
         )}
-      </a>
+      </button>
       <h1 className="text-3xl mb-10">MENU</h1>
       <div className="flex">
         {backEndData.map((item) => (
@@ -35,10 +60,16 @@ function App() {
             imgSrc={item.image}
             title={item.name}
             price={item.price}
-            btnFunc={(e) => addToCart(item.name)}
+            btnFunc={(e) => addToCart(item.name, item.price)}
           />
         ))}
       </div>
+      {cartItem.map((vals, i) => (
+        <>
+          <p>{vals.name}</p>
+          <p>{vals.price}</p>
+        </>
+      ))}
     </div>
   );
 }
