23e762134b082ab070b8482202ced96427364075
Teddy Pascual <teddylayson2000@gmail.com>
2024-01-19 12:09:41 +0800

    established authorization for add api endpoint

--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -1,5 +1,27 @@
 const Test = () => {
-  return <main className="h-screen bg-gray-800 text-white">Test</main>;
+  function sendData() {
+    const fileInp = document.getElementById("image");
+    const formData = new FormData();
+    formData.append("name", "Ted");
+    formData.append("image", fileInp.files[0]);
+    const send = () => {
+      fetch("api/test", {
+        method: "POST",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInBhc3N3b3JkIjoicGFzczEyMzQiLCJpYXQiOjE3MDU2MzUyMjd9.neKV9CQaPn4a4LtqM9GOkBwG0Lfam6ive8Y0cW9IZpo",
+        },
+        body: formData,
+      });
+    };
+    send();
+  }
+  return (
+    <main className="h-screen bg-gray-800 text-white">
+      <input type="file" name="image" id="image" />
+      <button onClick={sendData}>SEND</button>
+    </main>
+  );
 };
 
 export default Test;
