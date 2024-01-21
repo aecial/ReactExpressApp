65506ef378cbe49abcbd1a859029947c2282bdf9
Teddy Pascual <teddylayson2000@gmail.com>
2024-01-21 19:46:05 +0800

    improved contrast and styling of unauthorized component

--- client/src/components/Unauthorized.js
+++ client/src/components/Unauthorized.js
@@ -3,10 +3,12 @@ import { Button } from "@material-tailwind/react";
 const Unauthorized = () => {
   const navigate = useNavigate();
   return (
-    <main className=" bg-gray-800 text-white h-[100vh] flex flex-col gap-4 justify-center items-center">
+    <main className=" bg-black text-white h-[100vh] flex flex-col gap-4 justify-center items-center">
       <img src="https://http.cat/401.jpg" alt="Status Cat" />
       <h1 className="text-4xl">UNAUTHORIZED</h1>
-      <Button onClick={() => navigate("/")}>GO BACK</Button>
+      <Button onClick={() => navigate("/")} color="white">
+        GO BACK
+      </Button>
     </main>
   );
 };
